89e96d4e232569976c0e08cf621c12375fe6df8a
Shubhankar Salunke <shubhankar.salunke@gmail.com>
2026-04-04 23:22:42 +0530

    Moved connectors to root.

--- connector/aws.go
+++ connector/aws.go
@@ -3,25 +3,10 @@ package connector
 import (
 	"context"
 
+	"github.com/ShubhankarSalunke/lucifer/connectors"
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	stscreds "github.com/aws/aws-sdk-go-v2/credentials/stscreds"
-	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
 func ConnectAws(ctx context.Context, roleArn string, externalId string) (aws.Config, error) {
-	cfg, err := config.LoadDefaultConfig(ctx)
-	if err != nil{
-		return aws.Config{}, err
-	}
-
-	stsClient := sts.NewFromConfig(cfg)
-
-	provider := stscreds.NewAssumeRoleProvider(stsClient, roleArn, func(o *stscreds.AssumeRoleOptions) {
-		o.ExternalID = aws.String(externalId)
-	})
-
-	cfg.Credentials = aws.NewCredentialsCache(provider)
-
-	return cfg, nil
+	return connectors.ConnectAws(ctx, roleArn, externalId)
 }
